7e0fe02d5246f0d5d7c998a985adf1249590b1e7
vickysinghal <kumarvksinghal402@gmail.com>
2023-06-27 16:16:10 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -1,30 +1,5 @@
 $(document).ready(function() {
 
-  // Scroll to top button ----------------------------------------------------------
-  // When the user scrolls down 20px from the top of the document, show the button
-  window.onscroll = function() {
-    scrollFunction()
-  };
-
-  function scrollFunction() {
-    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
-      document.getElementById("topper").style.display = "block";
-    } else {
-      document.getElementById("topper").style.display = "none";
-    }
-  }
-
-  // When the user clicks on the button, scroll to the top of the document
-  function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  }
-
-  $("#topper").on("click", function() {
-    $("html").animate({
-      scrollTop: 0
-    }, 400);
-  });
 
   // tooltips function
   $(function() {
@@ -41,10 +16,10 @@ $(document).ready(function() {
         $("#read-more").fadeToggle("fast");
 
         // Change the button
-        if ($("#badge-more").text() == "more") {
-           $("#badge-more").text("less");
+        if ($("#badge-more").text() == "Read more...") {
+           $("#badge-more").text("Read less ");
         } else {
-           $("#badge-more").text("more");
+           $("#badge-more").text("Read more...");
         }
 
      });
